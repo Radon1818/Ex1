ae43b6846e5f462327c2bd397b62bb9a5b6c1ceb
Sergey Radimashvili <Sergey Radimashvili>
2016-05-05 02:16:18 +0300

    my first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var url = require('url');
+var students = require('./students');
+
+app.get('/StudentsList',function(req,res){
+	res.json(students.getAllStudents());
+});
+app.get('/StudentsAverage/:average',function(req,res){
+	res.json(students.getStudentByAverage(req.params.average));
+});
+app.get('/StudentsAverageById/:id',function(req,res){
+	res.json(students.getStudentAverageById(req.params.id));
+});
+app.get('/ExcellenceStudentsByYear/:year',function(req,res){
+	res.json(students.getExcellenceByYear(req.params.year));
+});
+
+app.listen(port);
+console.log("Listening on port - " + port);
---
+++ students.js
@@ -0,0 +1,41 @@
+var StudentsList = require('./students.json');
+
+exports.getAllStudents = function(){
+	return StudentsList.students[1].average;
+}
+
+exports.getStudentByAverage = function(average){
+	var arrey = new Array();
+	for(var i in StudentsList.students)
+	{
+		if(StudentsList.students[i].average == average)
+		{
+			arrey.push(StudentsList.students[i]);
+		}
+	}
+	return arrey;
+}
+
+exports.getStudentAverageById = function(id){
+	var arrey = new Array();
+	for(var i in StudentsList.students)
+	{
+		if(StudentsList.students[i].id == id)
+		{
+			arrey.push(StudentsList.students[i].average);
+		}
+	}
+	return arrey;
+}
+
+exports.getExcellenceByYear = function(year){
+	var arrey = new Array();
+	for(var i in StudentsList.students)
+	{
+		if((StudentsList.students[i].year == year)&&(StudentsList.students[i].average >= 85))
+		{
+			arrey.push(StudentsList.students[i]);
+		}
+	}
+	return arrey;
+}
